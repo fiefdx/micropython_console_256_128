7d9d9dd4fd55a839e0e70307d89d36be7d85e331
fiefdx <fiefdx@163.com>
2024-08-31 20:35:22 +0800

    update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ except:
     print("no multi-threading module support")
 from machine import Pin, SPI, PWM
 
-from ST7567 import ST7567
+from ST75256 import ST75256
 import sdcard
 # import font8
 import font7
@@ -29,7 +29,8 @@ from scheduler import Scheluder, Condition, Task, Message
 from common import ticks_ms, ticks_add, ticks_diff, sleep_ms
 from shell import Shell
 from keyboard import KeyBoard
-import bin
+sys.path.insert(0, "/bin")
+sys.path.append("/")
 
 if machine:
     machine.freq(240000000)
@@ -43,27 +44,42 @@ def monitor(task, name, scheduler = None, display_id = None):
     while True:
         gc.collect()
         # print(int(100 - (gc.mem_free() * 100 / (264 * 1024))), gc.mem_free())
-        monitor_msg = "CPU%s:%3d%%  RAM:%3d%%" % (scheduler.cpu, int(100 - scheduler.idle), int(100 - (scheduler.mem_free() * 100 / (264 * 1024))))
+        #monitor_msg = "CPU%s:%3d%%  RAM:%3d%%" % (scheduler.cpu, int(100 - scheduler.idle), int(100 - (scheduler.mem_free() * 100 / (264 * 1024))))
         #print(monitor_msg)
+        #print(len(scheduler.tasks))
         yield Condition(sleep = 2000)
         #yield Condition(sleep = 2000, send_msgs = [Message({"msg": monitor_msg}, receiver = display_id)])
 
 
 def display(task, name, scheduler = None, display_cs = None, sd_cs = None, spi = None):
     sd_cs.high()
-    spi.init(baudrate=8000000, polarity=1, phase=1)#under 20Mhz is OK
-    lcd = ST7567(spi, a0=Pin(1), cs=display_cs, rst=Pin(0), elecvolt=0x34, regratio=0x02, invX=False, invY=True, invdisp=0)
+    spi.init(baudrate=50000000, polarity=1, phase=1)
+    lcd = ST75256(256, 128, spi, Pin(1), Pin(6), display_cs, rot=0)
+    contrast = 0x138
+    contrast_max = 0x150
+    contrast_min = 0x120
+    lcd.contrast(contrast)
     frame_previous = None
-    clear_line = " " * 21
+    clear_line = " " * 42
     cursor_previous = None
     while True:
         yield Condition(sleep = 0, wait_msg = True)
         msg = task.get_message()
         sd_cs.high()
-        spi.init(baudrate=8000000, polarity=1, phase=1)#under 20Mhz is OK
+        spi.init(baudrate=50000000, polarity=1, phase=1)
         # time.sleep_ms(1)
         refresh = False
         #print(msg.content)
+        if "contrast" in msg.content:
+            if msg.content["contrast"] == "contrast-up":
+                contrast += 1
+                if contrast >= contrast_max:
+                    contrast = contrast_max
+            elif msg.content["contrast"] == "contrast-down":
+                contrast -= 1
+                if contrast <= contrast_min:
+                    contrast = contrast_min
+            lcd.contrast(contrast)
         if "clear" in msg.content:
             lcd.fill(0)
         if "frame" in msg.content:
@@ -98,9 +114,9 @@ def display(task, name, scheduler = None, display_cs = None, sd_cs = None, spi =
             for n, l in enumerate(lines):
                 if l:
                     Writer.set_textpos(lcd, n * 7, x)
-                    wri.printstring(clear_line)
+                    wri.printstring(clear_line, 0)
                     Writer.set_textpos(lcd, n * 7, x)
-                    wri.printstring(l)
+                    wri.printstring(l, 0)
             refresh = True
             frame_previous = frame
         if "cursor" in msg.content:
@@ -122,11 +138,11 @@ def display(task, name, scheduler = None, display_cs = None, sd_cs = None, spi =
         if "keyboard_mode" in msg.content:
             keyboard_mode = msg.content["keyboard_mode"]
             if keyboard_mode == "DF":
-                lcd.line(127, 0, 127, 5, 0)
+                lcd.line(1, 127, 10, 127, 0)
             elif keyboard_mode == "SH":
-                lcd.line(127, 0, 127, 2, 1)
+                lcd.line(1, 127, 5, 127, 1)
             elif keyboard_mode == "CP":
-                lcd.line(127, 3, 127, 5, 1)
+                lcd.line(6, 127, 10, 127, 1)
         #if scheduler.keyboard.mode == "DF":
         #    lcd.line(127, 0, 127, 5, 0)
         #elif scheduler.keyboard.mode == "SH":
@@ -135,18 +151,37 @@ def display(task, name, scheduler = None, display_cs = None, sd_cs = None, spi =
         #    lcd.line(127, 3, 127, 5, 1)
         if "bricks" in msg.content:
             refresh = True
+            offset_x = msg.content["bricks"]["offset_x"]
+            offset_y = msg.content["bricks"]["offset_y"]
             width = msg.content["bricks"]["width"]
             height = msg.content["bricks"]["height"]
             brick_size = msg.content["bricks"]["size"]
             data = msg.content["bricks"]["data"]
             for w in range(width):
-                x = w * brick_size
+                x = w * brick_size + offset_x
                 for h in range(height):
-                    y = h * brick_size
+                    y = h * brick_size + offset_y
                     if data[h][w] == "o":
                         lcd.rect(x, y, brick_size, brick_size, 0)
                     elif data[h][w] == "x":
                         lcd.rect(x, y, brick_size, brick_size, 1)
+        if "texts" in msg.content:
+            refresh = True
+            for text in msg.content["texts"]:
+                x = text["x"]
+                y = text["y"]
+                c = text["c"]
+                s = text["s"]
+                wri = Writer(lcd, font7)
+                #Writer.set_textpos(lcd, y, x)
+                #wri.printstring(c, 0)
+                Writer.set_textpos(lcd, y, x)
+                wri.printstring(s, 0)
+        if "rects" in msg.content:
+            refresh = True
+            for rect in msg.content["rects"]:
+                x, y, w, h = rect
+                lcd.rect(x, y, w, h, 1)
         if "binary" in msg.content:
             refresh = True
             data = msg.content["binary"]
@@ -189,6 +224,7 @@ def display(task, name, scheduler = None, display_cs = None, sd_cs = None, spi =
                         x += 1
         if refresh:
             lcd.show()
+        del msg
             
             
 def storage(task, name, scheduler = None, display_cs = None, sd_cs = None, spi = None):
@@ -211,18 +247,31 @@ def storage(task, name, scheduler = None, display_cs = None, sd_cs = None, spi =
             sd_cs.low()
             if "cmd" in msg.content:
                 cmd = msg.content["cmd"]
-                #print("cmd: ", cmd, len(cmd))
+                #print("cmd: ", cmd, len(cmd), sys.path)
                 args = cmd.split(" ")
                 module = args[0].split(".")[0]
                 #if "/sd/usr" not in sys.path:
                 #    sys.path.insert(0, "/sd/usr")
                 #import bin
-                import_str = "from bin import %s" % module
-                exec(import_str)
+                if module not in sys.modules:
+                    #import_str = "from bin import %s" % module
+                    import_str = "import %s; sys.modules['%s'] = %s" % (module, module, module)
+                    #print(import_str)
+                    exec(import_str)
+                #print(sys.modules)
                 if module in ("mount", "umount"):
-                    output, sd, vfs = bin.__dict__[module].main(*args[1:], shell_id = scheduler.shell_id, sd = sd, vfs = vfs, spi = spi, sd_cs = sd_cs)
+                    output, sd, vfs = sys.modules["%s" % module].main(*args[1:], shell_id = scheduler.shell_id, sd = sd, vfs = vfs, spi = spi, sd_cs = sd_cs)
                 else:
-                    output = bin.__dict__[module].main(*args[1:], shell_id = scheduler.shell_id)
+                    output = sys.modules["%s" % module].main(*args[1:], shell_id = scheduler.shell_id)
+                #exec("del bin.%s" % module)
+                exec("del %s" % module)
+                del sys.modules["%s" % module]
+                gc.collect()
+                #gc.mem_free()
+                #if module in ("mount", "umount"):
+                #    output, sd, vfs = bin.__dict__[module].main(*args[1:], shell_id = scheduler.shell_id, sd = sd, vfs = vfs, spi = spi, sd_cs = sd_cs)
+                #else:
+                #    output = bin.__dict__[module].main(*args[1:], shell_id = scheduler.shell_id)
                 yield Condition(sleep = 0, send_msgs = [
                     Message({"output": output}, receiver = scheduler.shell_id)
                 ])
@@ -230,6 +279,7 @@ def storage(task, name, scheduler = None, display_cs = None, sd_cs = None, spi =
             yield Condition(sleep = 0, send_msgs = [
                 Message({"output": str(e)}, receiver = scheduler.shell_id)
             ])
+        del msg
 
 
 def cursor(task, name, interval = 500, s = None, display_id = None, storage_id = None):
@@ -256,12 +306,13 @@ def cursor(task, name, interval = 500, s = None, display_id = None, storage_id =
                 yield Condition(sleep = interval, send_msgs = [Message({"cursor": (x, y, "hide")}, receiver = display_id)])
         else:
             yield Condition(sleep = interval)
+        del msg
         
         
 def shell(task, name, scheduler = None, display_id = None, storage_id = None):
     yield Condition(sleep = 1000)
     #s = Shell()
-    s = Shell(display_size = (20, 9), scheduler = scheduler, storage_id = storage_id, display_id = display_id)
+    s = Shell(display_size = (41, 18), scheduler = scheduler, storage_id = storage_id, display_id = display_id)
     s.write_line(" Welcome to TinyShell")
     s.write_char("\n")
     yield Condition(sleep = 0, send_msgs = [Message({"frame": s.get_display_frame()}, receiver = display_id)])
@@ -316,6 +367,7 @@ def shell(task, name, scheduler = None, display_id = None, storage_id = None):
             yield Condition(sleep = 0, send_msgs = [
                 Message(msg.content, receiver = display_id)
             ])
+        del msg
 
 
 def keyboard_input_test(task, name, interval = 50, shell_id = None):
@@ -343,14 +395,29 @@ def display_backlight(task, name, interval = 500, display_id = None):
             print("duty_cycle: ", duty_cycle)
             
             
-def keyboard_input(task, name, scheduler = None, interval = 50, shell_id = None):
+def keyboard_input(task, name, scheduler = None, interval = 50, shell_id = None, display_id = None):
     k = KeyBoard()
     scheduler.keyboard = k
     keyboard_mode = k.mode
+    key_sound = 2000
     while True:
         yield Condition(sleep = interval)
         key = k.scan()
-        if key != "":
+        #if len(keys) > 0:
+            #print(keys)
+            #for key in keys:
+        volume = k.get_volume()
+        if key.startswith("contrast"):
+            if volume > 0:
+                yield Condition(sleep = 0, send_msgs = [Message({"freq": key_sound, "volume": k.get_volume(), "length": 5}, receiver = scheduler.sound_id)])
+            yield Condition(sleep = 0, send_msgs = [Message({"contrast": key}, receiver = display_id)])
+        elif key in ("light-up", "light-down", "volume-up", "volume-down", "SH", "CP"):
+            if volume > 0:
+                yield Condition(sleep = 0, send_msgs = [Message({"freq": key_sound, "volume": k.get_volume(), "length": 5}, receiver = scheduler.sound_id)])
+        elif key != "":
+            # print("key: ", key)
+            if volume > 0:
+                yield Condition(sleep = 0, send_msgs = [Message({"freq": key_sound, "volume": k.get_volume(), "length": 5}, receiver = scheduler.sound_id)])
             if scheduler.shell and scheduler.shell.session_task_id and scheduler.exists_task(scheduler.shell.session_task_id):
                 yield Condition(sleep = 0, send_msgs = [Message({"msg": key}, receiver = scheduler.shell.session_task_id)])
             else:
@@ -359,6 +426,22 @@ def keyboard_input(task, name, scheduler = None, interval = 50, shell_id = None)
             keyboard_mode = k.mode
             yield Condition(sleep = 0, send_msgs = [Message({"keyboard_mode": keyboard_mode}, receiver = shell_id)])
         k.clear()
+        
+        
+def sound_output(task, name, scheduler = None, sound_pwm = None):
+    while True:
+        yield Condition(sleep = 0, wait_msg = True)
+        msg = task.get_message()
+        tone_freq = msg.content["freq"]
+        tone_length = msg.content["length"]
+        tone_volume = msg.content["volume"]
+        sound_pwm.freq(tone_freq)
+        sound_pwm.duty_u16(tone_volume)
+        if tone_length < 100:
+            sleep_ms(tone_length)
+        else:
+            yield Condition(sleep = tone_length)
+        sound_pwm.duty_u16(0)
 
 
 def counter(task, name, interval = 100, display_id = None):
@@ -384,6 +467,8 @@ def run_core1(task, name, scheduler = None, start_after = 5000, display_id = Non
 
 if __name__ == "__main__":
     try:
+        led = machine.Pin("LED", machine.Pin.OUT)
+        sound_pwm = PWM(Pin(12))
         sd_cs = machine.Pin(9, machine.Pin.OUT)
         display_cs = machine.Pin(5, machine.Pin.OUT)
         spi = SPI(0, baudrate=1000000, polarity=1, phase=1, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
@@ -391,15 +476,19 @@ if __name__ == "__main__":
         display_id = s.add_task(Task(display, "display", kwargs = {"scheduler": s, "display_cs": display_cs, "sd_cs": sd_cs, "spi": spi}))
         storage_id = s.add_task(Task(storage, "storage", kwargs = {"scheduler": s, "display_cs": display_cs, "sd_cs": sd_cs, "spi": spi}))
         # storage_id = None
+        sound_id = s.add_task(Task(sound_output, "sound_output", kwargs = {"scheduler": s, "sound_pwm": sound_pwm}))
+        s.sound_id = sound_id
         shell_id = s.add_task(Task(shell, "shell", kwargs = {"scheduler": s, "display_id": display_id, "storage_id": storage_id}))
         s.shell_id = shell_id
-        keyboard_id = s.add_task(Task(keyboard_input, "keyboard_input", kwargs = {"scheduler": s, "interval": 50, "shell_id": shell_id}))
+        keyboard_id = s.add_task(Task(keyboard_input, "keyboard_input", kwargs = {"scheduler": s, "interval": 10, "shell_id": shell_id, "display_id": display_id}))
         monitor_id = s.add_task(Task(monitor, "monitor", kwargs = {"scheduler": s, "display_id": display_id}))
         #counter_id = s.add_task(Task(counter, "counter", kwargs = {"interval": 10, "display_id": display_id}))
         #backlight_id = s.add_task(Task(display_backlight, "display_backlight", kwargs = {"interval": 500, "display_id": display_id}))
         #keyboard_id = s.add_task(Task(test_keyboard, "test_keyboard", kwargs = {"interval": 50, "display_id": display_id}))
+        # led.on()
+        led.off()
         s.run()
     except Exception as e:
         import sys
-        self.log("main exit: %s" % sys.print_exception(e))
+        print("main exit: %s" % sys.print_exception(e))
     print("core0 exit")
