e0736427bb2185cf9945bfc5d71c110e70142d12
fiefdx <fiefdx@163.com>
2025-04-04 18:33:31 -0600

    add optimized dict

---
+++ dictfile.py
@@ -0,0 +1,108 @@
+import os
+import json
+
+from basictoken import BASICToken as Token
+
+
+class DictFile(object):
+    def __init__(self, path):
+        self.path = path
+        self.wf = open(self.path, "w")
+        self.rf = open(self.path, "r")
+        self.index = {}
+
+    def writejson(self, d):
+        line = json.dumps({"d": d}) + "\n"
+        length = len(line)
+        pos = self.wf.tell()
+        self.wf.write(line)
+        self.wf.flush()
+        return pos, length
+
+    def keys(self):
+        return self.index.keys()
+
+    def __contains__(self, key):
+        return key in self.index
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, key):
+        pos, length = self.index[key]
+        self.rf = open(self.path, "r")
+        self.rf.seek(pos, 0)
+        d = json.loads(self.rf.read(length - 1))["d"]
+        t = []
+        if isinstance(d, list):
+            for dd in d:
+                t.append(Token(dd["c"], dd["C"], dd["l"]))
+        else:
+            t = Token(d["c"], d["C"], d["l"])
+        return t
+    
+    def get(self, key):
+        return self.__getitem__(key)
+
+    def __setitem__(self, key, value):
+        pos, length = self.writejson(value)
+        self.index[key] = [pos, length]
+
+    def clear(self):
+        self.index.clear()
+        self.rf.close()
+        self.wf.close()
+        self.wf = open(self.path, "w")
+        self.rf = open(self.path, "r")
+
+
+class DictFileSlow(object):
+    def __init__(self, path):
+        self.path = path
+        self.wf = open(self.path, "w")
+        self.rf = open(self.path, "r")
+        self.index = {}
+        
+    def writejson(self, d):
+        line = json.dumps({"d": d}) + "\n"
+        length = len(line)
+        pos = self.wf.tell()
+        self.wf.write(line)
+        self.wf.flush()
+        return pos, length
+
+    def keys(self):
+        return self.index.keys()
+
+    def __contains__(self, key):
+        return key in self.index
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, key):
+        pos = self.index[key]
+        self.rf = open(self.path, "r")
+        self.rf.seek(pos, 0)
+        d = json.loads(self.rf.readline()[:-1])["d"]
+        t = []
+        if isinstance(d, list):
+            for dd in d:
+                t.append(Token(dd["c"], dd["C"], dd["l"]))
+        else:
+            t = Token(d["c"], d["C"], d["l"])
+        return t
+    
+    def get(self, key):
+        return self.__getitem__(key)
+
+    def __setitem__(self, key, value):
+        pos, length = self.writejson(value)
+        self.index[key] = pos
+        
+    def clear(self):
+        self.index.clear()
+        self.rf.close()
+        self.wf.close()
+        self.wf = open(self.path, "w")
+        self.rf = open(self.path, "r")
