672e7ace453cfa546ca2d8e800a41a3cebdd1a32
fiefdx <fiefdx@163.com>
2025-04-14 08:20:59 -0600

    update ps command

--- bin/ps.py
+++ bin/ps.py
@@ -1,9 +1,15 @@
-import uos
+from scheduler import Condition, Task, Message
 
 
 def main(*args, **kwargs):
     scheduler = kwargs["scheduler"]
     result = ""
     for i, t in enumerate(scheduler.tasks):
-        result += "% 3d: %s\n"  % (t.id, t.name)
-    return result[:-1]
+        result += "%03d: %s\n"  % (t.id, t.name)
+    result += "-" * 42 + "\n"
+    result += "Message[%s/%s] Condition[%s/%s] Task[%s/%s]" % (
+        Message.remain(), len(Message.pool),
+        Condition.remain(), len(Condition.pool),
+        Task.remain(), len(Task.pool)
+    )
+    return result
