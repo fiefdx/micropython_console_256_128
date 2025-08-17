697965aecb2406486deb3d4caed4822fd0943bd5
fiefdx <fiefdx@gmail.com>
2025-08-17 09:55:16 -0600

    add ollama.py client

---
+++ ollama.py
@@ -0,0 +1,45 @@
+import os
+import time
+import json
+import urequests as requests
+
+
+class Chat(object):
+    def __init__(self, host, port, model, context_length = 10, timeout = 3600, stream = False):
+        self.host = host
+        self.port = port
+        self.model = model
+        self.timeout = timeout
+        self.stream = stream
+        self.context_length = context_length
+        self.url = "http://%s:%s/api/chat" % (host, port)
+        self.context = []
+        self.headers = {"Content-Type": "application/json"}
+
+    def chat(self, message):
+        self.context.append({"role": "user", "content": message})
+        if len(self.context) > self.context_length:
+            self.context.pop(0)
+        data = {"model": self.model, "messages": self.context, "stream": self.stream}
+        print(data)
+        r = requests.post(self.url, data = json.dumps(data), headers = self.headers, stream = self.stream)
+        print(dir(r.raw))
+        if r.status_code == 200:
+            if self.stream:
+                result = b""
+                line = r.raw.readline()
+                while line:
+                    result += line
+                    line = r.raw.readline()
+                return True, result
+            else:
+                response = r.json()
+                self.context.append(response["message"])
+                if len(self.context) > self.context_length:
+                    self.context.pop(0)
+                return True, response["message"]["content"]
+        else:
+            return False, r.reason
+
+    def clear(self):
+        self.context.clear()
