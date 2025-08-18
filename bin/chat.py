71db8ce5072aa11409dbddc14394438e0ee152d2
fiefdx <fiefdx@gmail.com>
2025-08-17 19:32:38 -0600

    update bin/chat.py

--- bin/chat.py
+++ bin/chat.py
@@ -50,56 +50,74 @@ class ChatShell(Shell):
     #     os.dupterm(self.term)
         
     def input_char(self, c):
-        if c == "\n":
-            cmd = self.cache[-1][len(self.prompt_c):].strip()
-            if len(cmd) > 0:
-                self.history.append(self.cache[-1][len(self.prompt_c):])
-                self.write_history(self.cache[-1][len(self.prompt_c):])
-                if cmd == "exit" or cmd == "quit":
-                    self.exit = True
-                elif cmd == "clear" or cmd == "new":
-                    self.chat.clear()
-                    self.write_lines("new chat", end = True)
+        try:
+            if c == "\n":
+                cmd = self.cache[-1][len(self.prompt_c):].strip()
+                if len(cmd) > 0:
+                    self.history.append(self.cache[-1][len(self.prompt_c):])
+                    self.write_history(self.cache[-1][len(self.prompt_c):])
+                    if cmd == "exit" or cmd == "quit":
+                        self.exit = True
+                    elif cmd == "clear":
+                        self.chat.clear()
+                        self.write_lines("new chat", end = True)
+                    elif cmd == "info":
+                        message = "host: %s\nport: %s\nmodel: %s\nctx: %s\n" % (self.chat.host, self.chat.port, self.chat.model, self.chat.context_length)
+                        self.write_lines(message, end = True)
+                    elif cmd.startswith("set model:"):
+                        self.chat.model = cmd.split(":")[-1].strip()
+                        self.write_lines("model: %s" % self.chat.model, end = True)
+                    elif cmd.startswith("set ctx:"):
+                        self.chat.context_length = int(cmd.split(":")[-1].strip())
+                        self.write_lines("ctx: %s" % self.chat.context_length, end = True)
+                    elif cmd.startswith("set host:"):
+                        self.chat.host = cmd.split(":")[-1].strip()
+                        self.write_lines("host: %s" % self.chat.host, end = True)
+                    elif cmd.startswith("set port:"):
+                        self.chat.port = cmd.split(":")[-1].strip()
+                        self.write_lines("port: %s" % self.chat.port, end = True)
+                    else:
+                        try:
+                            success, content = self.chat.chat(cmd)
+                            if success:
+                                self.write_lines(content, end = True)
+                            else:
+                                self.write_lines("fail reason: %s" % content.decode(), end = True)
+                        except Exception as e:
+                            self.write_lines(str(e), end = True)
                 else:
-                    try:
-                        success, content = self.chat.chat(cmd)
-                        if success:
-                            self.write_lines(content, end = True)
-                        else:
-                            self.write_lines("fail reason: %s" % content.decode(), end = True)
-                    except Exception as e:
-                        self.write_lines(str(e), end = True)
-            else:
-                self.cache.append(self.prompt_c)
-                self.cache_to_frame_history()
-            if len(self.history) > self.history_length:
-                self.history.pop(0)
-            self.history_idx = len(self.history)
-        elif c == "\b":
-            if len(self.cache[-1][:self.current_col]) > len(self.prompt_c):
-                self.cache[-1] = self.cache[-1][:self.current_col-1] + self.cache[-1][self.current_col:]
+                    self.cache.append(self.prompt_c)
+                    self.cache_to_frame_history()
+                if len(self.history) > self.history_length:
+                    self.history.pop(0)
+                self.history_idx = len(self.history)
+            elif c == "\b":
+                if len(self.cache[-1][:self.current_col]) > len(self.prompt_c):
+                    self.cache[-1] = self.cache[-1][:self.current_col-1] + self.cache[-1][self.current_col:]
+                    self.cursor_move_left()
+            elif c == "BX":
+                self.scroll_up()
+            elif c == "BB":
+                self.scroll_down()
+            elif c == "UP":
+                self.history_previous()
+            elif c == "DN":
+                self.history_next()
+            elif c == "LT":
                 self.cursor_move_left()
-        elif c == "BX":
-            self.scroll_up()
-        elif c == "BB":
-            self.scroll_down()
-        elif c == "UP":
-            self.history_previous()
-        elif c == "DN":
-            self.history_next()
-        elif c == "LT":
-            self.cursor_move_left()
-        elif c == "RT":
-            self.cursor_move_right()
-        elif c == "ES":
-            pass
-        elif len(c) == 1:
-            self.cache[-1] = self.cache[-1][:self.current_col] + c + self.cache[-1][self.current_col:]
-            self.cursor_move_right()
-                
-        if len(self.cache) > self.cache_lines:
-            self.cache.pop(0)
-        self.current_row = len(self.cache)
+            elif c == "RT":
+                self.cursor_move_right()
+            elif c == "ES":
+                pass
+            elif len(c) == 1:
+                self.cache[-1] = self.cache[-1][:self.current_col] + c + self.cache[-1][self.current_col:]
+                self.cursor_move_right()
+                    
+            if len(self.cache) > self.cache_lines:
+                self.cache.pop(0)
+            self.current_row = len(self.cache)
+        except Exception as e:
+            self.write_lines("error: %s" % str(e), end = True)
         
     def write_char(self, c):
         if c == "\n":
@@ -143,13 +161,11 @@ def main(*args, **kwargs):
         port = 11434
         stream = False
         if len(kwargs["args"]) > 0:
-            model = kwargs["args"][0]
+            host = kwargs["args"][0]
         if len(kwargs["args"]) > 1:
-            host = kwargs["args"][1]
+            port = kwargs["args"][1]
         if len(kwargs["args"]) > 2:
-            port = kwargs["args"][2]
-        if len(kwargs["args"]) > 3:
-            stream = True if int(kwargs["args"][3]) == 1 else False
+            stream = True if int(kwargs["args"][2]) == 1 else False
         s = ChatShell(display_size = (39, 17), host = host, port = port, model = model, stream = stream)
         shell.current_shell = s
         s.write_line("              Welcome to Chat")
